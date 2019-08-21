666233c06f8daf7bba035122504a0a54b64d3d19
Jeff Doolittle <jeffdoolittle@gmail.com>
2019-08-21 09:28:04 -0700

    wip

--- tempus_optimus.py
+++ tempus_optimus.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
 
-if __name__ == "__main__":
-    print("go!")
+import datetime, time
+
+year_rome_was_founded_bce = 753
+
+datetime.timedelta
+
+
+class Utc(datetime.tzinfo):
+    def fromutc(self, dt):
+        return dt
+
+    def utcoffset(self, dt):
+        return datetime.timedelta(0)
+
+    def dst(self, dt):
+        return datetime.timedelta(0)
+
+    def tzname(self, dt):
+        return "UTC"
 
 
 def optima_date():
@@ -10,4 +27,17 @@ def optima_date():
 
 def optima_time():
     pass
+
+
+if __name__ == "__main__":
+    current_year = datetime.datetime.now(Utc()).year - year_rome_was_founded_bce
+    print(current_year)
+
+    print(time.time())
+
+    print("UTC")
+    print(datetime.datetime.now(Utc()))
+
+    print("local")
+    print(datetime.datetime.now())
 
